f3dd07367c628efd3da07a1edc2cf2128adbe329
Avery Vine <averyvine@cmail.carleton.ca>
2017-03-26 12:49:19 -0400

    initial commit

---
+++ COMP2601A4Client-100999500Tests/COMP2601A4Client_100999500Tests.swift
@@ -0,0 +1,36 @@
+//
+//  COMP2601A4Client_100999500Tests.swift
+//  COMP2601A4Client-100999500Tests
+//
+//  Created by Avery Vine on 2017-03-26.
+//  Copyright © 2017 Avery Vine. All rights reserved.
+//
+
+import XCTest
+@testable import COMP2601A4Client_100999500
+
+class COMP2601A4Client_100999500Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
